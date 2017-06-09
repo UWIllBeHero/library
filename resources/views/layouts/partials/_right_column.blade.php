3c072faff1940ea11734d7f7dbf02e98b63f9c82
UWillBeHero <andriy.yendrizhyyevskyy@gmail.com>
2017-06-10 00:43:37 +0300

    view in progress

--- resources/views/layouts/partials/_right_column.blade.php
+++ resources/views/layouts/partials/_right_column.blade.php
@@ -1,3 +1,13 @@
-<div class="col-sm-2 sidenav">
-    <p><a href="#">Link</a></p>
-</div>
+<a href="/calendar">
+    <div class="col-sm-2 " style="font-family: 'Montserrat', sans-serif;">
+        <div class="col-sm-12" style="background: rgba(15,101,118,0.71);">
+            <h3 style="color: #fffcf9">понеділок</h3>
+        </div>
+        <div class="col-sm-12" style="background: rgba(255,252,249,0.71);">
+            <h1 style="color: #0f6576; font-size:6.2em;font-weight:500;">17</h1>
+        </div>
+        <div class="col-sm-12" style="background: rgba(15,101,118,0.71);">
+            <h3 style="color: #fffcf9">жовтня</h3>
+        </div>
+    </div>
+</a>
